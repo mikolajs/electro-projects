24c88e2ee7b032819542fe39f28b6f6e1ef41e7d
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2015-06-21 23:39:40 +0200

    main code for arduino vehicle

--- vehicle/arduino-code/bluetooth_test/bluetooth_test.ino
+++ vehicle/arduino-code/bluetooth_test/bluetooth_test.ino
@@ -7,29 +7,55 @@ void setup()
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() // run over and over
 {
     //String Data = "";
     //Serial.print(mySerial.read());
-    
-    while(mySerial.available()) {
-     character = mySerial.read();
-     //Data.concat(character);
-     mySerial.println(character);
-     Serial.println(character);
-     digitalWrite(2,1);
-     digitalWrite(3,1);
-     delay(1000);
-     
-     //count++;
+    if(mySerial.available()){
+      character = mySerial.read();
+      Serial.println(character);
+       switch(character) {
+         case 'a':
+         digitalWrite(9,0);
+         digitalWrite(10,1);
+         digitalWrite(11,0);
+         digitalWrite(12,1);
+         break;
+          case 'e':
+         digitalWrite(9,1);
+         digitalWrite(10,0);
+         digitalWrite(11,1);
+         digitalWrite(12,0);
+         break;
+          case 'd':
+         digitalWrite(9,0);
+         digitalWrite(10,1);
+         digitalWrite(11,1);
+         digitalWrite(12,0);
+         break;
+          case 'b':
+         digitalWrite(9,1);
+         digitalWrite(10,0);
+         digitalWrite(11,0);
+         digitalWrite(12,1);
+         break;
+         case 'c':
+         digitalWrite(9,0);
+         digitalWrite(10,0);
+         digitalWrite(11,0);
+         digitalWrite(12,0);
+         break;
+         default:
+         break;
+     }
     }
-    digitalWrite(2,0);
-     digitalWrite(3,0);
-    Serial.print("S\n");
-    mySerial.print("a\n");
-    delay(1000);
+     
+     delay(500);
+     
 }
