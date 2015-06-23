1767f0a9a5d5ddbeff1b61570312ed6fb4348eb0
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2015-06-23 23:25:30 +0200

    start weather project

---
+++ weather/index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var express = require('express');
+var app = express();
+var data = require('./database');
+var bodyParser = require('body-parser');
+
+
+app.use(express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/state', function(req, res){
+  res.send('{"ERROR":"not implemented"}');
+});
+
+app.get('/graph/:what', function(req, res){
+  var what = req.param('what');
+  if(what == "t") {
+     res.send('{"ERROR":"not implemented"}');
+  }
+  else if(what == "p") {
+    res.send('{"ERROR":"not implemented"}');
+  }
+  else if(what == 'h') res.send('{"ERROR":"not implemented"}');
+  else res.send('{"ERROR":"not recognized option"}');
+});
+
+
+
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+  setInterval(1000, function(){console.log("working");});
+
+});
