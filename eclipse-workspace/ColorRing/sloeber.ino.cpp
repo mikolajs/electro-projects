dc6844a96dc5edd1d627902e74b1f4c7300f9f11
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2017-12-09 17:55:43 +0100

    elipse proj – neopixels ring

---
+++ eclipse-workspace/ColorRing/sloeber.ino.cpp
@@ -0,0 +1,34 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-12-09 17:53:23
+
+#include "Arduino.h"
+#include "Arduino.h"
+#include <Adafruit_NeoPixel.h>
+
+void setup() ;
+void setColor(uint8_t nr, uint8_t r, uint8_t g, uint8_t b) ;
+void setRed(int nr, uint8_t pow) ;
+void setGreen(int nr, uint8_t pow) ;
+void setBlue(int nr, uint8_t pow) ;
+void setYellow(int nr, uint8_t pow) ;
+void setViolet(int nr, uint8_t pow) ;
+void setMarine(int nr, uint8_t pow) ;
+void setOrange(int nr, uint8_t pow) ;
+void switchOff(int nr) ;
+void lightDark() ;
+void rondo() ;
+void colorLight() ;
+boolean snakeIfOnLed(int i, uint8_t start, uint8_t end, uint8_t N) ;
+void snakeMove() ;
+void snakeMoveColor() ;
+void randomColor() ;
+void off() ;
+void loop() ;
+
+#include "ColorRing.ino"
+
+
+#endif
