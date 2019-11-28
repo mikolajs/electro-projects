645d9fd015b07d48bfde36a5c64c4d5aaee6affb
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2019-11-28 10:43:34 +0100

    esp8266 vehicle try

--- vehicle_esp8266/vehicle_esp8266.ino
+++ vehicle_esp8266/vehicle_esp8266.ino
@@ -13,7 +13,6 @@
 */
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <Servo.h>
 
 const char *ssid = "fablabauto1"; // SSID Name
 const char *password = "12345678"; // SSID Password : Between 8 and 32 carateres
@@ -26,74 +25,74 @@ ESP8266WebServer server(port);
 // Motors pins
 static const uint8_t motor_r = 0 ;
 static const uint8_t motor_l = 5;
-Servo servo_r;
-Servo servo_l;
 
 void setup() {
   WiFi.mode(WIFI_AP); //Only Access point
   WiFi.softAPConfig(ip, ip, netmask);
   WiFi.softAP(ssid, password);
-  servo_r.attach(motor_r);
-  servo_r.attach(motor_l);
   // Start Server
+  server.on("/command", HTTP_GET, handleCommand);
   server.on("/", HTTP_GET, handleRoot);
   server.begin();
 }
 
 void loop() {
-   servo_r.write(-360);
-  servo_l.write(-360);
+
   delay(1000);                                                                                                                                                                                                                                                                                          
   //server.handleClient();
 }
 
-void handleRoot() {
+void handleRoot(){
+  server.send(200, "text / plain", "Server Arduino");
+}
+
+void handleCommand() {
   if (server.hasArg("State")) {
     String command = server.arg("State");
     if (command.equals("F")) {
       forward();
       server.send(200, "text / plain", "Forward");
+      Serial.println("Forward");
     }
     else  if (command.equals("B")) {
       backward();
       server.send(200, "text / plain", "Backward");
+        Serial.println("Backward");
     }
     else  if (command.equals("L")) {
       turn_left();
       server.send(200, "text / plain", "Turn Left");
+        Serial.println("Left");
     }
     else  if (command.equals("R")) {
       turn_right();
       server.send(200, "text / plain", "Turn Right");
+        Serial.println("Rigth");
     }
     else  if (command.equals("S")) {
       stop_motors();
       server.send(200, "text / plain", "Stop");
+        Serial.println("Stop");
     }
   }
 }
 
 void stop_motors() {
-  servo_r.write(90);
-  servo_l.write(90);
+  
 }
 
 void backward() {
-  servo_r.write(-360);
-  servo_l.write(-360);
+ 
 }
 
 void forward() {
-  servo_r.write(360);
-  servo_l.write(360);
+ 
 }
 
 void turn_left() {
-  servo_r.write(360);
-  servo_l.write(-360);
+  
 }
 
 void turn_right() {
-  servo_r.write(-360);
-  servo_l.write(360);
+  
 }
