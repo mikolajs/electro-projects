2d69d2f94eb119bd6a5b2354322436c7e31e0103
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2024-06-05 20:32:45 +0200

    esp32 laser and fotodiode

--- esp32_wrooms/laser_test/laser_test.ino
+++ esp32_wrooms/laser_test/laser_test.ino
@@ -4,14 +4,21 @@
 */
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+    pinMode(2, OUTPUT);
     pinMode(0, OUTPUT);
+    pinMode(36, INPUT);
 }
 
 void loop() {
  digitalWrite(0, HIGH); 
+ digitalWrite(2, HIGH);
+ int foto = digitalRead(36);
+ if(foto == HIGH) Serial.println("Wysoki");
+ else Serial.println("Niski");
   delay(1000);
   digitalWrite(0, LOW); 
+  digitalWrite(2, LOW);
   delay(1000); 
 \
 }
